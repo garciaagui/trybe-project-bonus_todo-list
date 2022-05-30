175e76edca1e5d6fc9870c5a1cb5fa7ca81330d0
Guilherme Garcia <garciaguig@gmail.com>
2022-05-30 17:35:24 -0300

    Conclusão dos Requisitos 6, 7 e 8

--- script.js
+++ script.js
@@ -2,12 +2,24 @@ const addTaskInput = document.querySelector('#texto-tarefa');
 const addTaskButton = document.querySelector('#criar-tarefa');
 const taskList = document.querySelector('#lista-tarefas');
 
+function changeColor(event) {
+  const hadClassSelected = document.querySelector('.selected');
+  if (hadClassSelected) {
+    hadClassSelected.style.backgroundColor = 'white';
+    hadClassSelected.classList.remove('selected');
+  }
+  const auxiliar = event;
+  auxiliar.target.classList.add('selected');
+  auxiliar.target.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+
 function addNewTask() {
   const newTask = document.createElement('li');
   newTask.innerHTML = addTaskInput.value;
   newTask.setAttribute('class', 'task');
   taskList.appendChild(newTask);
   addTaskInput.value = '';
+  newTask.addEventListener('click', changeColor);
 }
 
 addTaskButton.addEventListener('click', addNewTask);
